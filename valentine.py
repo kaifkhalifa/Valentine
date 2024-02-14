b5d7824dd9e66e7f7a6debec5b103e053a571f75
Kaif Khalifa <81500937+kaifkhalifa@users.noreply.github.com>
2024-02-14 12:18:23 -0500

    Add files via upload

---
+++ valentine.py
@@ -0,0 +1,19 @@
+# Happy Valentine's Day message with a heart shape
+
+print("Happy Valentine's Day CS GANG!!\n")
+
+heart_shape = [
+    "  ***     ***  ",
+    " *****   ***** ",
+    "******* *******",
+    " ************* ",
+    "  ***********  ",
+    "   *********   ",
+    "    *******    ",
+    "     *****     ",
+    "      ***      ",
+    "       *       "
+]
+
+for line in heart_shape:
+    print(line)
